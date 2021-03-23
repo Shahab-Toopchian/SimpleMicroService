65e7695831a5453d9fe21887b00985a17ea2ed50
shahabzz <shahabtoopchian@gmail.com>
2021-03-24 02:31:36 +0430

    first cm

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Database;
+using UserService.Database.Entities;
+
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DatabaseContext _db;
+
+        public UserController(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _db.Users.ToList();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return _db.Users.Find(id);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                _db.Users.Add(model);
+                _db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created,model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;initial catalog=MicroService;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
